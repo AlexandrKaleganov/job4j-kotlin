8487aca3dca4613daa96538f76d954d8430e65cd
Alexandr KAleganov <alexmur07@mail.ru>
2020-09-16 15:36:54 +0700

    2. Типы данных и методы.[188826#361155]

---
+++ src/main/kotlin/ru/job4j/base/Calc.kt
@@ -0,0 +1,28 @@
+package ru.job4j.base
+
+fun additions(firs: Int, second: Int): Int {
+    return firs + second;
+}
+
+fun subtract(first: Int, second: Int): Int {
+    return first - second
+}
+
+fun multiplication(first: Int, second: Int): Int {
+    return first * second
+}
+
+fun doing(first: Int, second: Int): Int {
+    return first / second
+}
+
+fun main () {
+    val additions = additions(1, 1)
+    println("1 + 1 = $additions")
+    val subtract = subtract(1, 1)
+    println("1 - 1 = $subtract")
+    val multiplication = multiplication(2, 2)
+    println("2 * 2 = $multiplication")
+    val doing = doing(4, 2)
+    println("4 / 2 = $doing")
+}
