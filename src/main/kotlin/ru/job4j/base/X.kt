c1d47260b389632d4f087663586b2c13f3738024
Alexandr KAleganov <alexmur07@mail.ru>
2020-09-20 01:01:52 +0700

    5. Циклы while и for[189222#361158]

--- src/main/kotlin/ru/job4j/base/X.kt
+++ src/main/kotlin/ru/job4j/base/X.kt
@@ -1,16 +1,30 @@
 package ru.job4j.base
 
 fun draw(size: Int) {
-    if (size > 0 && size % 2 > 0)
+    if (isValidSize(size))
         for (value in 0 until size) {
-            (0 until size).forEach { value2 ->
-                if (value == value2 || value == size - 1 - value2) print("x") else print(" ")
+            for (value2 in 0 until size) {
+                if (value == value2 || value == size - 1 - value2) {
+                    print("x")
+                } else {
+                    print(" ")
+                }
             }
             println()
-        } else print("Операция не поддерживается")
+        }
+}
+
+fun isValidSize(size: Int): Boolean {
+    if (size > 0 && size % 2 > 0) {
+        return true
+    } else {
+        print("Операция не поддерживается")
+        return false
+    }
 }
 
 fun main() {
     draw(7)
     draw(10)
 }
+
