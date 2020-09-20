80dad83ab021d2e917e5fbcab67a57ea8da49278
Alexandr KAleganov <alexmur07@mail.ru>
2020-09-21 01:45:48 +0700

    5. Apply[338631#361176]

---
+++ src/main/kotlin/ru/job4j/lambda/HtmlTable.kt
@@ -0,0 +1,22 @@
+package ru.job4j.lambda
+
+class HtmlTable {
+    fun table(row: Int, cell: Int): String {
+        val table = StringBuilder()
+        table.apply {
+            append("<table>")
+            for (r in 0 until row) {
+                apply {
+                    append("<tr>")
+                    for (c in 0 until cell) {
+                        append("<td></td>")
+                    }
+                    append("</tr>")
+                }
+            }
+            append("</table>")
+        }
+        return table.toString()
+    }
+
+}
