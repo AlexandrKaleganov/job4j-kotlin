7b57002ccc95208344c36e6ca359d37d38571a35
Alexandr KAleganov <alexmur07@mail.ru>
2020-10-21 01:27:53 +0700

    8. lazy[338938#361186]

--- src/main/kotlin/ru/job4j/safe/lazy/LoadCityList.kt
+++ src/main/kotlin/ru/job4j/safe/lazy/LoadCityList.kt
@@ -1,7 +1,7 @@
 package ru.job4j.safe.lazy
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.jsoup.Jsoup
 
 class LoadCityList {
@@ -9,7 +9,7 @@ class LoadCityList {
 
     private fun getList (): List<City> {
         val doc = Jsoup.connect("https://gist.githubusercontent.com/gorborukov/0722a93c35dfba96337b/raw/435b297ac6d90d13a68935e1ec7a69a225969e58/russia").get()
-        return jacksonObjectMapper().readValue(doc.body().text(), object : TypeReference<List<City>>() {});
+        return jacksonObjectMapper().readValue(doc.body().text());
     }
 }
 fun main(args: Array<String>) {
